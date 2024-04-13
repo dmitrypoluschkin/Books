c49a981646aae1e7aa777ab2f2e310a51085bd57
unknown <19polushkin.dima87@gmail.com>
2024-04-13 17:43:24 +0400

    Books app

---
+++ web/bookr/bookr_test/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def greeting_view(request):
+    """Greet the user."""
+    return HttpResponse("Hey there, welcome to Bookr! Your one stop place to review books.")
